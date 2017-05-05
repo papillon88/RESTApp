2cdc58db043ae07e9d14ec95470ec6640282cd9f
papillon <devopreet.ch@gmail.com>
2017-05-04 22:20:24 -0500

    *more params : context, bean

--- src/main/java/com/eightyeight/papillon/endpoints/InjectDemoResource.java
+++ src/main/java/com/eightyeight/papillon/endpoints/InjectDemoResource.java
@@ -1,7 +1,10 @@
 package com.eightyeight.papillon.endpoints;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * Created by papillon on 5/4/2017.
@@ -18,6 +21,15 @@ public class InjectDemoResource {
                                           @HeaderParam("headerVal") String header,
                                           @CookieParam("name") String cookie){
         return "Matrix Param : " + matrixParam + " Header Param : "+ header;
+    }
+
+    @GET
+    @Path("/context")
+    public String getParamUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders headers){
+
+        String line = uriInfo.getAbsolutePath().toString();
+        String cooky = headers.getCookies().toString();
+        return line+" "+cooky;
     }
 
 
---
+++ src/main/java/com/eightyeight/papillon/endpoints/MessageFilterBean.java
@@ -0,0 +1,37 @@
+package com.eightyeight.papillon.endpoints;
+
+import javax.ws.rs.QueryParam;
+
+/**
+ * Created by papillon on 5/4/2017.
+ */
+public class MessageFilterBean {
+
+    private @QueryParam("year") int year;
+    private @QueryParam("start") int start;
+    private @QueryParam("size") int size;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
